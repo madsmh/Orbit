83de38407a1d53a81112ad2e48aba6d12c32d6c8
Mads M. Hansen <madsmh@gmail.com>
2017-07-08 19:51:00 +0200

    Have written the Body class and written some initial values for Earth.

--- orbit.py
+++ orbit.py
@@ -1,5 +1,4 @@
 import numpy as np
-# from scipy import integrate
 
 
 class Body:
@@ -25,18 +24,85 @@ class Body:
         self.vx0 = vx0
         self.vy0 = vy0
 
+        # Velocity (m/s)
+        self.vx = vx0
+        self.vy = vy0
+
         # Mass of the body (kg)
         self.M = mass
 
         # Radius of the body (m)
         self.radius = radius
 
-    def compute_accelaraton(self, x, y):
+    def compute_acceleration(self, x, y):
+        """Computes the gravitational acceleration due to self at position (x, y) (m)"""
 
-        # Acceleration in the x-direction
+        # Acceleration in the x-direction (m/s^2)
         ax = self.G*self.M/((self.x-x)**2 + (self.y-y)**2)*(self.x-x)/np.sqrt((self.x-x)**2 + (self.y-y)**2)
 
-        # Acceleration in the y-direction
+        # Acceleration in the y-direction (m/s^2)
         ay = self.G*self.M/((self.x-x)**2 + (self.y-y)**2)*(self.y-y)/np.sqrt((self.x-x)**2 + (self.y-y)**2)
 
         return ax, ay
+
+    def step(self, dt, target):
+        """4th order Runge-Kutta integration"""
+
+        # Acceleration due to target (tuple)
+        a = target.compute_acceleration(self.x, self.y)
+
+        k1x = self.vx
+        k1y = self.vy
+
+        k1vx = a[0]
+        k1vy = a[1]
+
+        k2x = self.vx + dt / 2 * k1vx
+        k2y = self.vy + dt / 2 * k1vy
+
+        # Acceleration due to target (tuple)
+        a = target.compute_acceleration(self.x + dt / 2 * k1x, self.y + dt / 2 * k1y)
+
+        k2vx = a[0]
+        k2vy = a[1]
+
+        k3x = self.vx + dt / 2 * k2vx
+        k3y = self.vy + dt / 2 * k2vy
+
+        # Acceleration due to target (tuple)
+        a = target.compute_acceleration(self.x + dt / 2 * k2x, self.y + dt / 2 * k2y)
+
+        k3vx = a[0]
+        k3vy = a[0]
+
+        k4x = self.vx + dt * k3vx
+        k4y = self.vy + dt * k3vy
+
+        # Acceleration due to target (tuple)
+        a = target.compute_acceleration(self.x + dt * k3x, self.y + dt * k3y)
+
+        k4vx = a[0]
+        k4vy = a[1]
+
+        # Update position
+        self.x = self.x + dt / 6 * (k1x + 2 * k2x + 2 * k3x + k4x)
+        self.x = self.x + dt / 6 * (k1y + 2 * k2y + 2 * k3y + k4y)
+
+        # Update velocity
+        self.vx = self.vx + dt / 6 * (k1vx + 2 * k2vx + 2 * k3vx + k4vx)
+        self.vy = self.vx + dt / 6 * (k1vy + 2 * k2vy + 2 * k3vy + k4vy)
+
+SUN_MASS = 1.989e30
+SUN_RADIUS = 695700000.0
+
+EARTH_X0 = -147095000000.0
+EARTH_Y0 = 0.0
+EARTH_VX0 = 0.0
+EARTH_VY0 = -30300.0
+EARTH_MASS = 5.972e24
+EARTH_RADIUS = 6371000.0
+dt = 86400.0
+
+sun = Body('Sun', 0, 0, 0, 0, SUN_MASS, SUN_RADIUS)
+earth = Body('Earth', EARTH_X0, EARTH_Y0, EARTH_VX0, EARTH_VY0, EARTH_MASS, EARTH_RADIUS)
+
