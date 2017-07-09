d36911cec1cc8bac01dd69697032441f1d45c816
Mads M. Hansen <madsmh@gmail.com>
2017-07-09 19:40:15 +0200

    Now it plots lines and integrates so all bodies' gravity is accounted for.

--- orbit.py
+++ orbit.py
@@ -40,9 +40,6 @@ JUPITER_VY0 = -13720.0
 JUPITER_MASS = 1898.19e24
 JUPUTER_RADIUS = 71492000.0
 
-# Time interval (1 Earth day)
-dt = 86400.0
-
 # Bodies
 sun = celestial.Body('Sun', 0, 0, 0, 0, SUN_MASS, SUN_RADIUS)
 earth = celestial.Body('Earth', EARTH_X0, EARTH_Y0, EARTH_VX0, EARTH_VY0, EARTH_MASS, EARTH_RADIUS)
@@ -50,87 +47,85 @@ jupiter = celestial.Body('Jupiter', JUPITER_X0, JUPITER_Y0, JUPITER_VX0, JUPITER
 mars = celestial.Body('Mars', MARS_X0, MARS_Y0, MARS_VX0, MARS_VY0, MARS_MASS, MARS_RADIUS)
 venus = celestial.Body('Venus', VENUS_X0, VENUS_Y0, VENUS_VX0, VENUS_VY0, VENUS_MASS, VENUS_RADIUS)
 
+# Arrays of Bodies
+earth_array = [sun, jupiter, mars, venus]
+jupiter_array = [sun, earth, mars, venus]
+mars_array = [sun, earth, jupiter, venus]
+venus_array = [sun, earth, jupiter, mars]
+
+# Number of coordinate pairs
+n = 4333
+
+# Time interval (1 Earth day)
+dt = 86400.0
+
+# NumPy arrays for coordinates
+trajectory_earth = np.zeros(shape=(n, 2))
+trajectory_mars = np.zeros(shape=(n, 2))
+trajectory_venus = np.zeros(shape=(n, 2))
+trajectory_jupiter = np.zeros(shape=(n, 2))
 
 # Generate coordinates
-def coords_earth():
-    """Generate x,y-coordinates of Earth"""
-    pos = np.zeros(shape=(365, 2))
-    for i in range(365):
-        pos[i][0] = earth.x
-        pos[i][1] = earth.y
-        earth.step(dt, sun)
-    return pos
+for i in range(n):
+    trajectory_earth[i][0] = earth.x
+    trajectory_earth[i][1] = earth.y
 
+    trajectory_mars[i][0] = mars.x
+    trajectory_mars[i][1] = mars.y
 
-def coords_jupiter():
-    """Generate x,y-coordinates of Jupiter"""
-    pos = np.zeros(shape=(4332, 2))
-    for i in range(4332):
-        pos[i][0] = jupiter.x
-        pos[i][1] = jupiter.y
-        jupiter.step(dt, sun)
-    return pos
+    trajectory_venus[i][0] = venus.x
+    trajectory_venus[i][1] = venus.y
 
+    trajectory_jupiter[i][0] = jupiter.x
+    trajectory_jupiter[i][1] = jupiter.y
 
-def coords_mars():
-    """Generate x,y-coordinates of Mars"""
-    pos = np.zeros(shape=(687, 2))
-    for i in range(687):
-        pos[i][0] = mars.x
-        pos[i][1] = mars.y
-        mars.step(dt, sun)
-    return pos
-
-
-def coords_venus():
-    """Generate x,y-coordinates of Venus"""
-    pos = np.zeros(shape=(225, 2))
-    for i in range(225):
-        pos[i][0] = venus.x
-        pos[i][1] = venus.y
-        venus.step(dt, sun)
-    return pos
-
-
-# Trajectories of the planets
-trajectory_earth = coords_earth()
-trajectory_mars = coords_mars()
-trajectory_venus = coords_venus()
-trajectory_jupiter = coords_jupiter()
+    earth.step(dt, earth_array)
+    mars.step(dt, mars_array)
+    venus.step(dt, venus_array)
+    jupiter.step(dt, jupiter_array)
 
 # Coordinates of the planets
 x_earth, y_earth = trajectory_earth.T
 x_mars, y_mars = trajectory_mars.T
 x_venus, y_venus = trajectory_venus.T
 x_jupiter, y_jupiter = trajectory_jupiter.T
-phi = np.linspace(0.0, 2*np.pi, 100)
-na = np.newaxis
-
-# Draw circles representing Earth
-x_line_earth = x_earth[na, :] + earth.radius * np.sin(phi[:, na])
-y_line_earth = y_earth[na, :] + earth.radius * np.cos(phi[:, na])
-
-# Draw circles representing Mars
-x_line_mars = x_mars[na, :] + mars.radius * np.sin(phi[:, na])
-y_line_mars = y_mars[na, :] + mars.radius * np.cos(phi[:, na])
-
-# Draw circles representing Venus
-x_line_venus = x_venus[na, :] + venus.radius * np.sin(phi[:, na])
-y_line_venus = y_venus[na, :] + venus.radius * np.cos(phi[:, na])
-
-# Draw circles representing Venus
-x_line_jupiter = x_jupiter[na, :] + jupiter.radius * np.sin(phi[:, na])
-y_line_jupiter = y_jupiter[na, :] + jupiter.radius * np.cos(phi[:, na])
-
-# Draw circle representing the Sun
-x_line_sun = sun.radius * np.sin(phi[:, na])
-y_line_sun = sun.radius * np.cos(phi[:, na])
 
 plt.axes().set_aspect('equal', 'datalim')
-plt.plot(x_line_earth, y_line_earth)
-plt.plot(x_line_sun, y_line_sun)
-plt.plot(x_line_mars, y_line_mars)
-plt.plot(x_line_venus, y_line_venus)
-plt.plot(x_line_jupiter, y_line_jupiter)
 
+plt.plot(x_earth, y_earth, 'g', linewidth = 0.5)
+plt.plot(x_mars, y_mars, 'r', linewidth = 0.5)
+plt.plot(x_venus, y_venus, 'b', linewidth = 0.5)
+plt.plot(x_jupiter, y_jupiter, 'r', linewidth = 0.5)
 plt.show()
+
+# phi = np.linspace(0.0, 2*np.pi, 100)
+# na = np.newaxis
+#
+# # Draw circles representing Earth
+# x_line_earth = x_earth[na, :] + earth.radius * np.sin(phi[:, na])
+# y_line_earth = y_earth[na, :] + earth.radius * np.cos(phi[:, na])
+#
+# # Draw circles representing Mars
+# x_line_mars = x_mars[na, :] + mars.radius * np.sin(phi[:, na])
+# y_line_mars = y_mars[na, :] + mars.radius * np.cos(phi[:, na])
+#
+# # Draw circles representing Venus
+# x_line_venus = x_venus[na, :] + venus.radius * np.sin(phi[:, na])
+# y_line_venus = y_venus[na, :] + venus.radius * np.cos(phi[:, na])
+#
+# # Draw circles representing Venus
+# x_line_jupiter = x_jupiter[na, :] + jupiter.radius * np.sin(phi[:, na])
+# y_line_jupiter = y_jupiter[na, :] + jupiter.radius * np.cos(phi[:, na])
+#
+# # Draw circle representing the Sun
+# x_line_sun = sun.radius * np.sin(phi[:, na])
+# y_line_sun = sun.radius * np.cos(phi[:, na])
+#
+#
+# plt.plot(x_line_earth, y_line_earth)
+# plt.plot(x_line_sun, y_line_sun)
+# plt.plot(x_line_mars, y_line_mars)
+# plt.plot(x_line_venus, y_line_venus)
+# plt.plot(x_line_jupiter, y_line_jupiter)
+
+
