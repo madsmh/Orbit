969bb4b530428ef7ff39d28f515c960d048dfb3d
Mads M. Hansen <madsmh@gmail.com>
2017-07-11 00:22:57 +0200

    Added some 3d line plots of some of the orbits. The orbits are not correct.

--- orbit3d.py
+++ orbit3d.py
@@ -1,4 +1,6 @@
 import numpy as np
+# import matplotlib as mpl
+# from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import celestial3d
 import read_horizon
@@ -17,7 +19,7 @@ MERCURY_RADIUS = 2439700
 VENUS_MASS = 4.8675e24
 VENUS_RADIUS = 6051800.0
 
-MARS_MASS = 6.4171e23
+MARS_MASS = 6.4185e23
 MARS_RADIUS = 3389500.0
 
 JUPITER_MASS = 1898.19e24
@@ -32,7 +34,7 @@ URANUS_RADIUS = 24973000
 NEPTUNE_MASS = 102.413e24
 NEPTUNE_RADIUS = 24341000
 
-POLUTO_MASS = 1.307e22
+PLUTO_MASS = 1.307e22
 PLUTO_RADIUS = 1195e3
 
 earth_data = read_horizon.readdata('earth')
@@ -48,14 +50,14 @@ mars_data = read_horizon.readdata('mars')
 # Bodies
 sun = celestial3d.Body('Sun', 0, 0, 0, 0, 0, 0, SUN_MASS, SUN_RADIUS)
 earth = celestial3d.Body('Earth', *earth_data[0][:], EARTH_MASS, EARTH_RADIUS)
-jupiter = celestial3d.Body('Jupiter', *jupiter_data, JUPITER_MASS, JUPUTER_RADIUS)
+jupiter = celestial3d.Body('Jupiter', *jupiter_data[0][:], JUPITER_MASS, JUPUTER_RADIUS)
 mars = celestial3d.Body('Mars', *mars_data[0][:], MARS_MASS, MARS_RADIUS)
 venus = celestial3d.Body('Venus', *venus_data[0][:], VENUS_MASS, VENUS_RADIUS)
 mercury = celestial3d.Body('Mercury', *mercury_data[0][:], MERCURY_MASS, MERCURY_RADIUS)
 saturn = celestial3d.Body('Saturn', *saturn_data[0][:], SATURN_MASS, SATURN_RADIUS)
 uranus = celestial3d.Body('Uranus', *uranus_data[0][:], URANUS_MASS, URANUS_RADIUS)
 neptune = celestial3d.Body('Neptune', *neptune_data[0][:], NEPTUNE_MASS, NEPTUNE_RADIUS)
-pluto = celestial3d.Body('pluto', *pluto_data[0][:], NEPTUNE_MASS, NEPTUNE_RADIUS)
+pluto = celestial3d.Body('Pluto', *pluto_data[0][:], PLUTO_MASS, PLUTO_RADIUS)
 
 
 # Arrays of Bodies
@@ -71,10 +73,10 @@ neptune_array = [sun, earth, jupiter, mars, venus, mercury, saturn, uranus, plut
 pluto_array = [sun, earth, jupiter, mars, venus, mercury, saturn, uranus, neptune]
 
 # Number of coordinate pairs
-n = 8*600
+n = 16*687
 
 # Time interval in seconds (1/8 Earth day)
-dt = 86400.0/8
+dt = 86400.0/16
 
 # NumPy arrays for coordinates
 trajectory_earth = np.zeros(shape=(n, 3))
@@ -141,58 +143,46 @@ def gen_coords():
         saturn.step(dt, saturn_array)
         uranus.step(dt, uranus_array)
         neptune.step(dt, neptune_array)
-        pluto.step(dt, neptune_array)
+        pluto.step(dt, pluto_array)
 
-# gen_coords()
+gen_coords()
 
-# TODO Must create z-coords for the bodies below -- if needed.
+# TODO Create x, y, z-coordinates for pluto
 # Coordinates of the planets
-x_earth, y_earth = trajectory_earth.T
-x_mars, y_mars = trajectory_mars.T
-x_venus, y_venus = trajectory_venus.T
-x_jupiter, y_jupiter = trajectory_jupiter.T
-x_mercury, y_mercury = trajectory_mercury.T
-x_sun, y_sun = trajectory_sun.T
-x_saturn, y_saturn = trajectory_saturn.T
-x_uranus, y_uanus = trajectory_uranus.T
-x_neptune, y_neptune = trajectory_neptune.T
+x_earth, y_earth, z_earth = trajectory_earth.T
+x_mars, y_mars, z_mars = trajectory_mars.T
+x_venus, y_venus, z_venus = trajectory_venus.T
+x_jupiter, y_jupiter, z_jupiter = trajectory_jupiter.T
+x_mercury, y_mercury, z_mercury = trajectory_mercury.T
+x_sun, y_sun, z_sun = trajectory_sun.T
+x_saturn, y_saturn, z_saturn = trajectory_saturn.T
+x_uranus, y_uanus, z_uranus = trajectory_uranus.T
+x_neptune, y_neptune, z_neptune = trajectory_neptune.T
 
 # TODO Write the 3D plotting for the planets and the sun.
-plt.axes().set_aspect('equal', 'datalim')
 
-plt.plot(x_earth, y_earth, 'g', linewidth=0.5)
-plt.plot(x_mars, y_mars, 'r', linewidth=0.5)
-plt.plot(x_venus, y_venus, 'b', linewidth=0.5)
-plt.plot(x_jupiter, y_jupiter, 'k', linewidth=0.5)
-plt.plot(x_mercury, y_mercury, 'k', linewidth=0.5)
-plt.plot(x_sun, y_sun, 'k')
-plt.plot(x_saturn, y_saturn, 'b', linewidth=0.5)
-plt.plot(x_uranus, y_uanus, 'k', linewidth=0.5)
-plt.plot(x_neptune, y_neptune, 'r', linewidth=0.5)
+fig = plt.figure()
+ax = fig.gca(projection='3d')
 
+ax.plot(x_earth, y_earth, z_earth, label=earth.name)
+ax.plot(x_mars, y_mars, z_mars, label=mars.name)
+# ax.plot(x_mercury, y_mercury, z_mercury, label=mercury.name)
+ax.plot(x_venus, y_venus, z_venus, label=venus.name)
+lim = 1.5e11
 
-# Plot circles representing the planets
-phi = np.linspace(0.0, 2*np.pi, 100)
-na = np.newaxis
+ax.auto_scale_xyz([-lim, lim], [-lim, lim], [-lim, lim])
 
-x_line_sun = x_sun[-1] + sun.radius * np.sin(phi[:, na])
-y_line_sun = y_sun[-1] + sun.radius * np.cos(phi[:, na])
-
-x_line_earth = x_earth[-1] + earth.radius * np.sin(phi[:, na])
-y_line_earth = y_earth[-1] + earth.radius * np.cos(phi[:, na])
-
-x_line_jupiter = x_jupiter[-1] + jupiter.radius * np.sin(phi[:, na])
-y_line_jupiter = y_jupiter[-1] + jupiter.radius * np.cos(phi[:, na])
-
-
-plt.plot(x_line_earth, y_line_earth)
-plt.plot(x_line_sun, y_line_sun, 'y')
-plt.plot(x_line_jupiter, y_line_jupiter, 'b')
-plt.plot(x_line_sun, y_line_sun, 'y')
-
-
+ax.legend()
 plt.show()
 
+# plt.plot(x_mars, y_mars, 'r', linewidth=0.5)
+# plt.plot(x_venus, y_venus, 'b', linewidth=0.5)
+# plt.plot(x_jupiter, y_jupiter, 'k', linewidth=0.5)
+# plt.plot(x_mercury, y_mercury, 'k', linewidth=0.5)
+# plt.plot(x_sun, y_sun, 'k')
+# plt.plot(x_saturn, y_saturn, 'b', linewidth=0.5)
+# plt.plot(x_uranus, y_uanus, 'k', linewidth=0.5)
+# plt.plot(x_neptune, y_neptune, 'r', linewidth=0.5)
 #
 # # Draw circles representing Mars
 # x_line_mars = x_mars[na, :] + mars.radius * np.sin(phi[:, na])
