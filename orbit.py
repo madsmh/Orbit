49b7f3bb7b40fec35ad1532c2afd635c169ef9d5
Mads M. Hansen <madsmh@gmail.com>
2017-07-10 12:42:45 +0200

    Added planet Murcury.

--- orbit.py
+++ orbit.py
@@ -17,7 +17,8 @@ MERCURY_X0 = -46.0e9
 MERCURY_Y0 = 0.0
 MERCURY_VX0 = 0.0
 MERCURY_VY0 = -58980.0
-MERCURY_MASS = 0.33011e24
+MERCURY_MASS = 3.3011e23
+MERCURY_RADIUS = 2439700
 
 VENUS_X0 = -107480000000.0
 VENUS_Y0 = 0.0
@@ -40,55 +41,89 @@ JUPITER_VY0 = -13720.0
 JUPITER_MASS = 1898.19e24
 JUPUTER_RADIUS = 71492000.0
 
+SATURN_X0 = -1352550000000.0
+SATURN_Y0 = 0.0
+SATURN_VX0 = 0.0
+SATURN_VY0 = -10180.0
+SATURN_MASS = 568.34e24
+SATURN_RADIUS = 7149200
+
 # Bodies
 sun = celestial.Body('Sun', 0, 0, 0, 0, SUN_MASS, SUN_RADIUS)
 earth = celestial.Body('Earth', EARTH_X0, EARTH_Y0, EARTH_VX0, EARTH_VY0, EARTH_MASS, EARTH_RADIUS)
 jupiter = celestial.Body('Jupiter', JUPITER_X0, JUPITER_Y0, JUPITER_VX0, JUPITER_VY0, JUPITER_MASS, JUPUTER_RADIUS)
 mars = celestial.Body('Mars', MARS_X0, MARS_Y0, MARS_VX0, MARS_VY0, MARS_MASS, MARS_RADIUS)
 venus = celestial.Body('Venus', VENUS_X0, VENUS_Y0, VENUS_VX0, VENUS_VY0, VENUS_MASS, VENUS_RADIUS)
+mercury = celestial.Body('Mercury', MERCURY_X0, MERCURY_Y0, MERCURY_VX0, MARS_VY0, MERCURY_MASS, MERCURY_RADIUS)
+saturn = celestial.Body('Saturn', SATURN_X0, SATURN_Y0, SATURN_VX0, SATURN_VY0, SATURN_MASS, SATURN_RADIUS)
 
 # Arrays of Bodies
-earth_array = [sun, jupiter, mars, venus]
-jupiter_array = [sun, earth, mars, venus]
-mars_array = [sun, earth, jupiter, venus]
-venus_array = [sun, earth, jupiter, mars]
+sun_array = [mercury, earth, jupiter, mars, venus, saturn]
+earth_array = [sun, jupiter, mars, venus, mercury, saturn]
+jupiter_array = [sun, earth, mars, venus, mercury, saturn]
+mars_array = [sun, earth, jupiter, venus, mercury, saturn]
+venus_array = [sun, earth, jupiter, mars, mercury, saturn]
+mercury_array = [sun, earth, jupiter, mars, venus, saturn]
+saturn_array = [sun, earth, jupiter, mars, venus, mercury]
 
 # Number of coordinate pairs
-n = 4333
+n = 8*600
 
-# Time interval (1 Earth day)
-dt = 86400.0
+# Time interval in seconds (1/8 Earth day)
+dt = 86400.0/8
 
 # NumPy arrays for coordinates
 trajectory_earth = np.zeros(shape=(n, 2))
 trajectory_mars = np.zeros(shape=(n, 2))
 trajectory_venus = np.zeros(shape=(n, 2))
 trajectory_jupiter = np.zeros(shape=(n, 2))
+trajectory_mercury = np.zeros(shape=(n, 2))
+trajectory_sun = np.zeros(shape=(n, 2))
+trajectory_saturn = np.zeros(shape=(n, 2))
+
 
 # Generate coordinates
-for i in range(n):
-    trajectory_earth[i][0] = earth.x
-    trajectory_earth[i][1] = earth.y
+def gen_coords():
+    for i in range(n):
+        trajectory_earth[i][0] = earth.x
+        trajectory_earth[i][1] = earth.y
 
-    trajectory_mars[i][0] = mars.x
-    trajectory_mars[i][1] = mars.y
+        trajectory_mars[i][0] = mars.x
+        trajectory_mars[i][1] = mars.y
 
-    trajectory_venus[i][0] = venus.x
-    trajectory_venus[i][1] = venus.y
+        trajectory_venus[i][0] = venus.x
+        trajectory_venus[i][1] = venus.y
 
-    trajectory_jupiter[i][0] = jupiter.x
-    trajectory_jupiter[i][1] = jupiter.y
+        trajectory_jupiter[i][0] = jupiter.x
+        trajectory_jupiter[i][1] = jupiter.y
 
-    earth.step(dt, earth_array)
-    mars.step(dt, mars_array)
-    venus.step(dt, venus_array)
-    jupiter.step(dt, jupiter_array)
+        trajectory_mercury[i][0] = mercury.x
+        trajectory_mercury[i][1] = mercury.y
+
+        trajectory_sun[i][0] = sun.x
+        trajectory_sun[i][1] = sun.y
+
+        trajectory_saturn[i][0] = saturn.x
+        trajectory_saturn[i][1] = saturn.y
+
+        earth.step(dt, earth_array)
+        mars.step(dt, mars_array)
+        venus.step(dt, venus_array)
+        jupiter.step(dt, jupiter_array)
+        mercury.step(dt, mercury_array)
+        sun.step(dt, sun_array)
+        saturn.step(dt, saturn_array)
+
+gen_coords()
 
 # Coordinates of the planets
 x_earth, y_earth = trajectory_earth.T
 x_mars, y_mars = trajectory_mars.T
 x_venus, y_venus = trajectory_venus.T
 x_jupiter, y_jupiter = trajectory_jupiter.T
+x_mercury, y_mercury = trajectory_mercury.T
+x_sun, y_sun = trajectory_sun.T
+x_saturn, y_saturn = trajectory_saturn.T
 
 plt.axes().set_aspect('equal', 'datalim')
 
@@ -96,6 +131,9 @@ plt.plot(x_earth, y_earth, 'g', linewidth=0.5)
 plt.plot(x_mars, y_mars, 'r', linewidth=0.5)
 plt.plot(x_venus, y_venus, 'b', linewidth=0.5)
 plt.plot(x_jupiter, y_jupiter, 'k', linewidth=0.5)
+plt.plot(x_mercury, y_mercury, 'k', linewidth=0.5)
+plt.plot(x_sun, y_sun, 'k')
+plt.plot(x_saturn, y_saturn, 'b', linewidth=0.5)
 plt.show()
 
 # phi = np.linspace(0.0, 2*np.pi, 100)
