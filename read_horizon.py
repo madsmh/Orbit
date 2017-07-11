44d2893c628064b28b34416cffea1ff037c92869
Mads M. Hansen <madsmh@gmail.com>
2017-07-11 15:52:06 +0200

    Excluded the header from the array.

--- read_horizon.py
+++ read_horizon.py
@@ -5,8 +5,7 @@ def readdata(planet):
     path = 'data/' + planet + '.csv'
     with open(path) as csvfile:
         datareader = csv.reader(csvfile, delimiter=',', skipinitialspace=True)
-        headers=next(datareader)
-        data.append(headers[2:8])
+        next(datareader)
         for row in datareader:
             #Convert srings to floats and to m/s and m.
             floatrow = [float(i)*1000 for i in row[2:8]]
