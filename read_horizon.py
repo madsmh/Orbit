a5bcf7cb1265a502fc6da58338782679fb4f7b52
Mads M. Hansen <madsmh@gmail.com>
2017-07-14 17:23:25 +0200

    Now using Numpy to read the data.

--- read_horizon.py
+++ read_horizon.py
@@ -1,26 +1,14 @@
-import csv
+import numpy as np
 
 def readdata(planet):
-    data = []
     path = 'data/' + planet + '.csv'
-    with open(path) as csvfile:
-        datareader = csv.reader(csvfile, delimiter=',', skipinitialspace=True)
-        next(datareader)
-        for row in datareader:
-            #Convert srings to floats and to m/s and m.
-            floatrow = [float(i)*1000 for i in row[2:8]]
-            data.append(floatrow)
+    data = np.loadtxt(path, delimiter=',', dtype=np.float64, skiprows=1, usecols=range(2, 8))
+    data = data*1e3
     return data
 
 
 def readdiagnosticdata(planet):
-    data = []
     path = 'diagnostic_data/' + planet + '.csv'
-    with open(path) as csvfile:
-        datareader = csv.reader(csvfile, delimiter=',', skipinitialspace=True)
-        next(datareader)
-        for row in datareader:
-            #Convert srings to floats and to m/s and m.
-            floatrow = [float(i)*1000 for i in row[2:8]]
-            data.append(floatrow)
+    data = np.loadtxt(path, delimiter=',', dtype=np.float64, skiprows=1, usecols=range(2, 8))
+    data = data * 1e3
     return data
