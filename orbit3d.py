839062f52bfe728f85178cd4977a24fa0d1770cf
Mads M. Hansen <madsmh@gmail.com>
2017-07-15 00:27:50 +0200

    Have implemented the force matrix along with various member functions in Body.

--- orbit3d.py
+++ orbit3d.py
@@ -1,9 +1,7 @@
 import numpy as np
 
-
 class System:
     def __init__(self, bodies):
-
         self.bodies = bodies
         self.n = len(self.bodies)
 
@@ -12,6 +10,33 @@ class System:
         for b, i in zip(self.bodies, range(self.n)):
             pos[i][:] = b.get_position()
         return pos
+
+    def force_matrix(self):
+
+        def force(body1, body2):
+            """Force acting on body1 from body2"""
+
+            g = 6.67408e-11
+            pos1 = body1.get_position()
+            pos2 = body2.get_position()
+
+            # Avoid a divide by zero
+            if pos1 == pos1:
+                return np.array([0, 0, 0])
+
+            r12 = pos2 - pos1
+            dist = np.abs(r12)
+            r12_hat = r12 / dist
+
+            return -g * body1.mass * body2.mass / (dist ** 2) * r12_hat
+
+        forces = np.zeros(shape=(self.n, self.n, 3))
+
+        for one, i in zip(self.bodies, range(self.n)):
+            for two, j in zip(self.bodies, range(self.n)):
+                forces[j][i][:] = force(one, two)
+
+        return forces
 
 
 class Body:
@@ -50,25 +75,26 @@ class Body:
         # Radius of the body (m)
         self.radius = radius
 
-    def compute_acceleration(self, x, y, z):
-        """Computes the gravitational acceleration due to self at position (x, y) (m)"""
-        # Deltas
-        delta_x = self.x - x
-        delta_y = self.y - y
-        delta_z = self.z - z
-
-        # Distance squared
-        r2 = delta_x ** 2 + delta_y ** 2 + delta_z ** 2
-        # Distance
-        r = np.sqrt(r2)
-
-        f = self.GM / r2
-
-        # Return numpy array with the acceleration
-        return np.array([f * delta_x / r, f * delta_y / r, f * delta_z / r])
+    def compute_acceleration(self, force):
+        """Calculate the acceleration given the resultant force (vector)"""
+        return force/self.mass
 
     def get_position(self):
         return np.array([self.x, self.y, self.z])
 
+    def get_velocity(self):
+        return np.array([self.vx, self.vy, self.vz])
+
+    def set_position(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def set_velocity(self, vx, vy, vz):
+        self.vx = vx
+        self.vy = vy
+        self.vz = vz
+
     def ke(self):
+        """Calculate the kinetic energy"""
         return 0.5 * self.mass * (self.vx ** 2 + self.vy ** 2 + self.vz ** 2)
