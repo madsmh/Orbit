d688f99efb45f43be65eb39b7163a7cb098588ac
Mads M. Hansen <madsmh@gmail.com>
2017-07-14 14:07:38 +0200

    Added function to load diagnostic data.

--- read_horizon.py
+++ read_horizon.py
@@ -13,3 +13,14 @@ def readdata(planet):
     return data
 
 
+def readdiagnosticdata(planet):
+    data = []
+    path = 'diagnostic_data/' + planet + '.csv'
+    with open(path) as csvfile:
+        datareader = csv.reader(csvfile, delimiter=',', skipinitialspace=True)
+        next(datareader)
+        for row in datareader:
+            #Convert srings to floats and to m/s and m.
+            floatrow = [float(i)*1000 for i in row[2:8]]
+            data.append(floatrow)
+    return data
